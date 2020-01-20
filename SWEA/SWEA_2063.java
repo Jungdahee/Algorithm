e3419563f9425c34ba887bf613490ecfa1fad12a
Jung dahee <jungdahee0108@gmail.com>
2020-01-21 00:13:02 +0900

    20.01.21 - 중간값 찾기

---
+++ SWEA/SWEA_2063.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SWEA_2063 { //중간값 찾기
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int num[] = new int[n];
+		
+		for(int i = 0; i < n; i++) num[i] = sc.nextInt();
+		
+		Arrays.sort(num);
+		System.out.println(num[(n - 1) / 2]);
+	}
+}
