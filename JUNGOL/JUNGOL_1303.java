bdd8883439b2d597d17b615ba7935dde21b1d9a9
Jung dahee <jungdahee0108@gmail.com>
2020-01-26 20:59:29 +0900

    20.01.26 - 숫가사각형1

--- JUNGOL/JUNGOL_1303.java
+++ JUNGOL/JUNGOL_1303.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class JUNGOL_1303{ //숫자사각형1
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String data[] = br.readLine().split(" ");
         int n = Integer.parseInt(data[0]);
         int m = Integer.parseInt(data[1]);
@@ -11,5 +11,5 @@ public class JUNGOL_1303{ //숫자사각형1
             if(i % m == 0 && i != 0) System.out.println();
             System.out.print((i + 1) + " ");
         }
-	}
+    }
 }
