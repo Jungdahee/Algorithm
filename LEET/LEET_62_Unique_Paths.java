7a4c270d82f5f58dd259c82f1707527a6c35cdeb
Jung dahee <jungdahee0108@gmail.com>
2020-12-27 16:48:31 +0900

    20.12.27 - Unique

---
+++ LEET/LEET_62_Unique_Paths.java
@@ -0,0 +1,19 @@
+public class LEET_62_Unique_Paths {
+    public static int uniquePaths(int m, int n) {
+        int map[][] = new int[m][n];
+
+        for(int i = 0; i < m; i++) map[i][0] = 1;
+        for(int i = 0; i < n; i++) map[0][i] = 1;
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                map[i][j] = map[i][j - 1] + map[i - 1][j];
+            }
+        }
+        return map[m - 1][n - 1];
+    }
+
+    public static void main(String args[]){
+        System.out.println(uniquePaths(3, 7));
+    }
+}
