f834b06a4214ab7420e11b78c0dfb5f92a17d086
Jung dahee <jungdahee0108@gmail.com>
2020-12-27 16:50:27 +0900

    20.12.27 - Unique_Paths

--- LEET/LEET_62_Unique_Paths.java
+++ LEET/LEET_62_Unique_Paths.java
@@ -2,6 +2,7 @@ public class LEET_62_Unique_Paths {
     public static int uniquePaths(int m, int n) {
         int map[][] = new int[m][n];
 
+        // 첫 행과 첫 열 1로 세팅(그쪽으로 올 수 있는 경우의 수는 1이므로)
         for(int i = 0; i < m; i++) map[i][0] = 1;
         for(int i = 0; i < n; i++) map[0][i] = 1;
 
